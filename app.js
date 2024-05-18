064b215ea97c51cef1c4c9c35eef3d379e0eca36
YasminMenezes06 <yasmin.758688@graduacao.fsa.br>
2024-05-17 21:52:13 -0300

    Create app.js
    Criação do arquivo app JS verifica-se o numero par ou impar

---
+++ app.js
@@ -0,0 +1,11 @@
+// Definindo uma lista de números
+let numeros= [2,4,6,8,10]
+// Percorrendo a lista com um loop for 
+for (let i = 0; i < numeros.lenght; i++){
+  // verificando se o número é par ou impar 
+  if (numero(i) % 2 === 0) {
+    console.log(numero(i) + " é um numero par);
+    )else(
+      console.log(numero(i)+ " é um numero impar);
+   }
+}
